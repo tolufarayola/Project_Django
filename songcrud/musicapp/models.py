fd220b561d14e6661224d04336263a2d262dc478
Tolulope Farayola <farayolatolulope@gmail.com>
2022-10-27 17:03:20 +0100

    My first django project

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+from turtle import title
+# from unittest.util import _MAX_LENGTH
+# from django.db import models
 
-# Create your models here.
+# # Create your models here.
+# class Artiste(models.Model):
+#     first_name = models.CharField(max_length = 400)
+#     last_name = models.CharField(max_length = 400)
+#     age = models.BigIntegerField()
+
+# class Song(models.Models):
+#     Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+#     title = models.CharField(max_length = 400)
+#     date_released = models.DateField()
+#     likes = models.IntegerField()
+#     artiste_id = models.BigAutoField()
+
+# class Lyric(models.Models):
+#     Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+#     context = models.CharField(max_length = 4000)
+#     song_id = models.IntegerField()
